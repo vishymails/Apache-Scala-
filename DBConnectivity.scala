aec100a7799b179b163886fe6dfe8b4342a82c84
vishymails <vishymails@gmail.com>
2019-05-15 15:07:13 +0530

    Add files via upload

---
+++ DBConnectivity.scala
@@ -0,0 +1,36 @@
+import java.sql.DriverManager
+import java.sql.Connection
+
+
+object ScalaJdbcConnectSelect {
+
+  def main(args: Array[String]) {
+    // connect to the database named "mysql" on the localhost
+    val driver = "com.mysql.jdbc.Driver"
+    val url = "jdbc:mysql://localhost/mysql"
+    val username = "root"
+    val password = "db2admin"
+
+    // there's probably a better way to do this
+    var connection:Connection = null
+
+    try {
+      // make the connection
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+
+      // create the statement, and run the select query
+      val statement = connection.createStatement()
+      val resultSet = statement.executeQuery("SELECT host, user FROM user")
+      while ( resultSet.next() ) {
+        val host = resultSet.getString("host")
+        val user = resultSet.getString("user")
+        println("host, user = " + host + ", " + user)
+      }
+    } catch {
+      case e => e.printStackTrace
+    }
+    connection.close()
+  }
+}
+
